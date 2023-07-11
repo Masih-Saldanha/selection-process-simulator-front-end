83eedee42235678fd1ec2d48fe74339be672faad
Masih <masih.saldanha@gmail.com>
2023-07-11 19:31:55 -0300

    feat: add control-candidates component basic logic

--- src/app/pages/control-candidates/control-candidates.component.ts
+++ src/app/pages/control-candidates/control-candidates.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./control-candidates.component.css']
 })
 export class ControlCandidatesComponent {
+  codCandidato: number | "" = "";
+  errorMessage = "";
 
+  onInput(inputElement: HTMLInputElement) {
+    const value = parseInt(inputElement.value);
+    if (value) {
+      this.errorMessage = "";
+    }
+
+    this.codCandidato = value;
+  }
+
+  disqualify() {
+    console.log(this.codCandidato);
+
+    // IMPLEMENTAR LÓGICA DE REQUISIÇÃO E DE MENSAGEM DE ERRO
+  }
+
+  schedule() {
+    console.log(this.codCandidato);
+
+    // IMPLEMENTAR LÓGICA DE REQUISIÇÃO E DE MENSAGEM DE ERRO
+  }
+
+  approve() {
+    console.log(this.codCandidato);
+
+    // IMPLEMENTAR LÓGICA DE REQUISIÇÃO E DE MENSAGEM DE ERRO
+  }
 }
